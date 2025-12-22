f7cf815acb0971e4738d2ee49bff94112b9e9ee8
mijanur-rahman-oli <sheikhmijanurrahmanoli@gmail.com>
2025-12-22 14:10:30 +0600

    Club

---
+++ src/pages/Club/Club.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Clubs from '../../components/Home/Clubs'
+
+const Club = () => {
+  return (
+    <div> 
+      <Clubs/>
+    </div>
+  )
+}
+
+export default Club
